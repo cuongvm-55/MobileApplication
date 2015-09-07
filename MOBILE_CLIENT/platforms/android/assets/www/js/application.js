816fc253e4ac6faff933e9ed2d01e40655c1e40f
datnq55 <datnq.55@gmail.com>
2015-09-07 21:48:06 +0700

    - Rename hello service to movie services - Add a little css for account popup - Generate movie list by javascript and read movies from real server - Modify DatabaseTags

--- MOBILE_CLIENT/platforms/android/assets/www/js/application.js
+++ MOBILE_CLIENT/platforms/android/assets/www/js/application.js
@@ -17,22 +17,121 @@
  * under the License.
  */
 
+var ROOT_URL  = "http://env-3166114.jelastic.servint.net/rest/movieservice/movieList";
+var LIST_MOVIE = [];
+var STARTING_POINT = 0;
+
+function Movie() {
+   this.id = "";
+   this.name = "";
+   this.director = "";
+   this.actor = "";
+   this.year = "";
+   this.status = "";
+   this.thumbnail = "";
+   this.description = "";
+}
+
+/* Function to get the HTML for each item*/
+function getItemHTML(kindItem, movieItem) {
+   var itemHTML = "<li ";
+   switch(kindItem) {
+      case 1: /* This is the first child */
+         itemHTML += "class='ui-li-has-thumb ui-first-child'>";
+         break;
+      case 2: /* This is the last child */
+         itemHTML += "class='ui-li-has-thumb ui-last-child'>";
+         break;
+      default:
+         itemHTML += "class='ui-li-has-thumb'>";
+         break;
+   }
+   itemHTML += "<a class='movie-button ui-btn ui-btn-icon-right ui-icon-carat-r'>";
+   itemHTML += "<img src=" + "'" + movieItem.thumbnail + "'" + " class='ui-li-thumb' />";
+   itemHTML += "<h2>" + movieItem.name + "</h2>";
+   itemHTML += "<div class='movie-description'>";
+   itemHTML += "<h3>" + movieItem.actor + "</h3>";
+   itemHTML += "<p>" + "90 phut" + "</p>";
+   itemHTML += "</div>";
+   itemHTML += "<p class='ui-li-aside'>" + movieItem.status + "</p>";
+   itemHTML += "</a></li>";
+
+   return itemHTML;
+}
+
+/* Function to get the HTML for list of items */
+function getListHTML(starting_point, movie_length) {
+   var listHTML = "";
+
+   if(movie_length + starting_point > LIST_MOVIE.length) {
+      movie_length = LIST_MOVIE.length - starting_point;
+   }
+   if(movie_length > 0) {
+      listHTML += getItemHTML(1, LIST_MOVIE[starting_point]);
+
+      for(var i=starting_point + 1; i<starting_point + movie_length-1; ++i) {
+         listHTML += getItemHTML(3, LIST_MOVIE[i]);
+      }
+
+      listHTML += getItemHTML(2, LIST_MOVIE[starting_point + movie_length - 1]);
+   }
+   return listHTML;
+}
+
+function attachListMovie(jsondata) {
+   $.each(jsondata, function(key, value) {
+      console.log(value.ID);
+      var movieItem = new Movie();
+   	movieItem.id = value.ID;
+   	movieItem.name = value.NAME;
+   	movieItem.director = value.DIRECTOR;
+   	movieItem.actor = value.ACTOR;
+   	movieItem.year = value.YEAR;
+   	movieItem.status = value.STATUS;
+   	movieItem.thumbnail = value.THUMBNAIL;
+   	movieItem.description = value.DESCRIPTION;
+
+   	LIST_MOVIE.push(movieItem);
+   });
+   console.log(LIST_MOVIE.length);
+}
+
 var Application  = {
    initApplication: function() {
-      Application.initMovieList();
+      $.ajax({
+         type: "GET",
+         url: ROOT_URL,
+         datatype: "json",
+         success: function(jsondata) {
+            attachListMovie(jsondata);
+            Application.initMovieList();
+         }
+      });
+
       Application.initSearchButton();
 
       /* Back button listener */
       $(document).on("backbutton", function() {
-         $("#movie-content").load("movie-list.html #movie-list", function () {
-            $('.movie-button').click(function(){
-               $("#movie-content").load("movie-details.html #movie-frame");
-            });
-         });
+         Application.initMovieList();
       });
    },
    initMovieList: function() {
       $("#movie-content").load("movie-list.html #movie-list", function () {
+         STARTING_POINT = 0;
+         $("#movie-list").append(getListHTML(STARTING_POINT, 12));
+         STARTING_POINT = STARTING_POINT + 12;
+
+         $(window).scroll(function () {
+            console.log('scroll');
+            var height = $(window).height();
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop + height >= (height - 100)) {
+               /* now load more list-items because the user is within 100px of the bottom of the page */
+               console.log('You Hit Bottom!');
+               $("#movie-list").append(getListHTML(STARTING_POINT, 6));
+               STARTING_POINT = STARTING_POINT + 6;
+            }
+         });
          $('.movie-button').click(function(){
             $("#movie-content").load("movie-details.html #movie-frame");
          });
